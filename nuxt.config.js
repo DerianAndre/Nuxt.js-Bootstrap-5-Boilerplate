35022aa8ff304108f41e6e7f932a78ac18d4b39f
André Castillo <hola@derianandre.com>
2021-09-10 19:22:39 -0600

    Removed unnecessary custom styles from old project, added montserrat font, using bs variables for heading mod.

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
       { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' },
     ],
     script: [
