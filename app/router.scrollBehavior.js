ad0281ceba5b03decb38c3e709da27ad0d2a331c
André Castillo <hola@derianandre.com>
2021-11-05 13:19:18 -0700

    Create router.scrollBehavior.js

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,30 @@
+export default async function (to, from, savedPosition) {
+	if (savedPosition) {
+		return savedPosition;
+	}
+
+	const findEl = async (hash, x = 0) => {
+		return (
+			document.querySelector(hash) ||
+			new Promise(resolve => {
+			if (x > 50) {
+				return resolve(document.querySelector("#app"));
+			}
+			setTimeout(() => {
+				resolve(findEl(hash, ++x || 1));
+			}, 100);
+			})
+		);
+	};
+
+	if (to.hash) {
+		let element = await findEl(to.hash);
+		if ("scrollBehavior" in document.documentElement.style) {
+			return window.scrollTo({ top: element.offsetTop - 65, behavior: "smooth" });
+		} else {
+			return window.scrollTo(0, element.offsetTop);
+		}
+	}
+
+	return { x: 0, y: 0 };
+}
