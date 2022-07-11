36ad6590e458ee11bb834059fb9369d7eeb71f31
Moritz Hamberger <mo@freiraum.co>
2022-07-11 11:43:41 +0200

    title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Bootstrap 5",
+    title: "Gasthaus Lamm - Willkommen auf der Schwäbischen Alb",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
