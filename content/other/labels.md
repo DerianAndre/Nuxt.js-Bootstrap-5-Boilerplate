3f242e6de155c4568dbf6b7bd1544f6008f57e67
Moritz Hamberger <mo@freiraum.co>
2022-07-13 09:26:21 +0200

    labels

---
+++ content/other/labels.md
@@ -0,0 +1,8 @@
+---
+labels:
+- "/images/labels/haus_der_bw_weine_logo_4c.jpeg"
+- "/images/labels/sds_logo_gastro_klein.jpeg"
+- "/images/labels/signet_mitglied_im_dehoga__72dpi__jpg.jpeg"
+- "/images/labels/sqd_logo_mitsz_4c_1.jpeg"
+
+---
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,27 @@
   <main>
     <HeroHome :page="page" />
     <nuxt-content :document="page" />
+    <section style="background-color: #fff">
+      <b-container>
+        <b-row class="align-items-center justify-content-around py-3">
+          <b-col cols="3" md="1" v-for="(label, i) in labels.labels" :key="i">
+            <b-img :src="label" class="img-fluid"></b-img>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
     <NewsSection :news="news" />
   </main>
 </template>
 
 <script>
 export default {
-  async asyncData({ $content, params }) {
+  async asyncData({ $content, params, error }) {
     try {
       const page = await $content("index").fetch();
       const news = await $content("news").limit(4).fetch();
-      return { news, page };
+      const labels = await $content("other/labels").fetch();
+      return { news, page, labels };
     } catch (err) {
       console.error({ err });
       error({
