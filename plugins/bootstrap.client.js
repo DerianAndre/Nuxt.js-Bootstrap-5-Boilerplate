731957d3f8a11e30be15aad24066e5453999b724
André Castillo <hola@derianandre.com>
2022-08-06 21:29:33 -0500

    Update bootstrap.client.js
    With this modification it is possible to use bootstrap with `$boostrap` this fixes some problem for exmaple with bootstrap scrollspy when changing from one page to another (in the old way and using data-bs-*) it will stop working. But with this you can apply scrollspy in mounted with javascript as the example says.

--- plugins/bootstrap.client.js
+++ plugins/bootstrap.client.js
@@ -1,1 +1,11 @@
-import bootsrtap from "bootstrap"
+import * as bootsrtap from "bootstrap";
+
+export default ({ app }, inject) => {
+  inject('bootstrap', bootsrtap);
+  //  This will allow us to use the bootstrap object
+  //
+  //  Example:
+  //  const scrollSpy = new this.$bootstrap.ScrollSpy(document.body, {
+  //    target: '#scrollspy'
+  //  });
+}
